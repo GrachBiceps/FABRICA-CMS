94e4d680d6189f7c266aa3611e4b5955678be09c
kanatchikov <62083304+kanatchikov@users.noreply.github.com>
2021-07-28 17:07:46 +0900

    vue i

---
+++ frontend/src/form.vue
@@ -0,0 +1,27 @@
+<template>
+<div >
+    <ul>
+        <li v-for="item in array" :key="item">
+            {{ item.name }}
+            {{item.coutn}}
+            
+  </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    data(){ 
+        return{
+    array:[{name:"fdfd",coutn:"0"},{name:"fdfd",coutn:"1"},{name:"fdfd",coutn:"23"},{name:"fdfd",coutn:"34"}]
+   
+    }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
