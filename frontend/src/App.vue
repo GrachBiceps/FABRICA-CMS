9d8b6c32369577299f4a57e6fae646f9ede9fff2
Акобян Г.K <grachbicepsbf4@gmail.com>
2021-07-30 14:40:13 +0900

    TopBar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="app" style="display:flex">
       <Navbar></Navbar>
-      <router-view></router-view>
+      <div style="width:100%">
+        <MyTopBar></MyTopBar>
+        <router-view></router-view>
+      </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/NavBar";
+import MyTopBar from "@/components/MyTopBar";
 
 export default {
     components: {
-      Navbar
+      Navbar, MyTopBar
     }
 }
 </script>
