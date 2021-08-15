e0e84e41243e5a5383735d8e2203265707b02b7c
Акобян Г.K <grachbicepsbf4@gmail.com>
2021-08-15 17:27:36 +0900

    Update Orders.vue

--- frontend/src/pages/Orders.vue
+++ frontend/src/pages/Orders.vue
@@ -1,12 +1,12 @@
 <template>
-<div>
-    <div class="flexmy ">
-       <button class="mybtn d-block btn-user " v-on:click="showdivin">
-          Заказы 
-       </button>
-       <button class="mybtn d-block btn-user" v-on:click="showdivout">
-          Расходы 
-       </button>
+  <div>
+    <div class="flexmy">
+      <button class="mybtn d-block btn-user" v-on:click="showdivin">
+        Заказы
+      </button>
+      <button class="mybtn d-block btn-user" v-on:click="showdivout">
+        Расходы
+      </button>
     </div>
     <div v-show="OrderInShow">
       <transition name="fade">
@@ -22,50 +22,51 @@
 </template>
 
 <script>
-import OrderIn from "@/components/OrderIn";
-import OrderOut from "@/components/OrderOut";
+import OrderIn from '@/components/OrderIn';
+import OrderOut from '@/components/OrderOut';
 
-    export default {
-    components: {
-      OrderOut, OrderIn
+export default {
+  components: {
+    OrderOut,
+    OrderIn,
+  },
+  data() {
+    return {
+      OrderInShow: false,
+      OrderOutShow: false,
+    };
+  },
+  methods: {
+    showdivin() {
+      (this.OrderInShow = true), (this.OrderOutShow = false);
     },
-      data () {
-        return {
-          OrderInShow: false,
-          OrderOutShow: false
-        }
-      },
-      methods:  {
-        showdivin () {
-          this.OrderInShow = true,
-          this.OrderOutShow = false
-        },
-        showdivout () {
-          this.OrderInShow = false,
-          this.OrderOutShow = true
-        }
-      }
-    }
+    showdivout() {
+      (this.OrderInShow = false), (this.OrderOutShow = true);
+    },
+  },
+};
 </script>
 
 <style>
-.flexmy{
+.flexmy {
   display: flex;
 }
-.mybtn{
+.mybtn {
   background-color: #5779df;
-  text-align:start;
+  text-align: start;
   border-radius: 0;
   color: rgba(255, 255, 255, 0.76);
   height: 50px;
   width: 50%;
   text-align: center;
   margin: 10px;
-}.mybtn:hover{
-  color: white; 
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .2s;
+.mybtn:hover {
+  color: white;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
   opacity: 0;
