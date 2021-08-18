473a40d3737488ba09933daf8b4206ee5823a457
kanatchikov <62083304+kanatchikov@users.noreply.github.com>
2021-08-18 14:45:05 +0900

    Update StorageINShow.vue

--- frontend/src/components/Storage/Storage-Accountant/StorageINShow.vue
+++ frontend/src/components/Storage/Storage-Accountant/StorageINShow.vue
@@ -38,12 +38,12 @@
         <div class="bg-white shadow-lg rounded-lg mx-auto my-auto" >
           <div class="head_lenta flex text-white h-9"><div class="w-full my-auto text-center">Редактирование</div></div>
           <tr class="grid grid-cols-12 gap-1 m-4">
-            <td class="my-auto col-span-3 w-full h-4/5"><input v-model="name"  type=text class="text-center w-full h-full" placeholder="Название"></td>
-            <td class="my-auto col-span-2 w-full h-4/5"><input v-model="count" type=number class="text-center w-full h-full" placeholder="Колличество"></td>
-            <td class="my-auto col-span-2 w-full h-4/5"><input v-model="price" type=number class="text-center w-full h-full" placeholder="Цена"></td>
-            <td class="my-auto col-span-2 w-full h-4/5"><input v-model="amount" type=number class="text-center w-full h-full" placeholder="Сумма"></td>
+            <td class="my-auto col-span-3 w-full h-4/5"><input v-model="nameModal"  type=text class="text-center w-full h-full" placeholder="Название"></td>
+            <td class="my-auto col-span-2 w-full h-4/5"><input v-model="countModal" type=number class="text-center w-full h-full" placeholder="Колличество"></td>
+            <td class="my-auto col-span-2 w-full h-4/5"><input v-model="priceModal" type=number class="text-center w-full h-full" placeholder="Цена"></td>
+            <td class="my-auto col-span-2 w-full h-4/5"><input v-model="amountModal" type=number class="text-center w-full h-full" placeholder="Сумма"></td>
             <td class="flex grid-cols-4 gap-1 col-span-3">
-              <button class="p-2 w-full col-span-3 my-auto shadow-sm rounded-lg bg-green-400 text-white">Сохранить</button>
+              <button @click="saveModal()" class="p-2 w-full col-span-3 my-auto shadow-sm rounded-lg bg-green-400 text-white">Сохранить</button>
               <button @click="editpos(false)" class="p-2 w-full  my-auto shadow-sm rounded-lg bg-red-400 text-white">Отмена</button>
             </td>
           </tr> 
@@ -66,7 +66,12 @@
         price:"",
         amount:"",
 
-        amountOfItems: 0,
+        idModal:"",
+        nameModal:"",
+        countModal:"",
+        priceModal:"",
+        amountModal:"",
+
         editModal: false, 
          tbData: []
       }
@@ -88,8 +93,20 @@
       })
       },
       editpos(string){
-        (this.editModal = string)
-        }
+        this.editModal = string
+        this.idModal = this.tbData[string-1].id
+        this.nameModal = this.tbData[string-1].name
+        this.countModal = this.tbData[string-1].count
+        this.priceModal = this.tbData[string-1].price
+        this.amountModal = this.tbData[string-1].amount
+      },
+      saveModal(){        
+        this.tbData[this.idModal-1].name = this.nameModal 
+        this.tbData[this.idModal-1].count = this.countModal 
+        this.tbData[this.idModal-1].price =  this.priceModal 
+        this.tbData[this.idModal-1].amount = this.amountModal 
+        this.editModal = false
+      }
     }
     
     }
